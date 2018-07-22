e44e4be02d012295e645aeac6e64019fab890852
Aman Kishore <amankishore105@gmail.com>
2018-07-22 02:34:54 -0500

    First method

---
+++ src/news_api.py
@@ -0,0 +1,23 @@
+import requests
+import json
+import datetime
+import dateutil.relativedelta
+
+class ApiClient:
+
+    # date = datetime.date + dateutil.relativedelta.relativedelta(months=-6)
+
+    def __init__(self, api_key):
+        self.base_url = 'https://newsapi.org/v2/'
+        self.api_key = api_key
+        self.api_key_url = '&apiKey=' + self.api_key
+
+    def get_news_past_six_months(self, keyword):
+
+        data = requests.get(self.base_url+'everything?'+'q='+keyword+'&from=2018-01-30'+'&sortBy=popularity'+self.api_key_url)
+        if(data.ok):
+            jsonData = json.loads(data.content)
+            return jsonData
+        else:
+            print("Unsuccesful: " + str(data.status_code))
+            return None
--- src/test.py
+++ src/test.py
@@ -1,11 +1,14 @@
 import requests
+import json
 import numpy
 import pandas
 import sklearn
 import matplotlib
 import seaborn
 import scipy
+import news_api
 
+news = news_api.ApiClient('9c964579f7ea4465855d943ba98552f8')
 
 url = ('https://newsapi.org/v2/everything?'
        'q=Fortnite&'
@@ -14,5 +17,5 @@ url = ('https://newsapi.org/v2/everything?'
        'apiKey=9c964579f7ea4465855d943ba98552f8')
 
 response = requests.get(url).json()
-
-print(response)
+re = news.get_news_past_six_months('fortnite')
+print(re)
