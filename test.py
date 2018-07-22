4236ca487e733c04d8d57310937a9fe402db5e8d
Aman Kishore <amankishore105@gmail.com>
2018-07-22 00:18:15 -0500

    Initial Commit

---
+++ test.py
@@ -0,0 +1,19 @@
+import requests
+import numpy
+import pandas
+import sklearn
+import matplotlib
+import seaborn
+import scipy
+
+import requests
+
+url = ('https://newsapi.org/v2/everything?'
+       'q=Fortnite&'
+       'from=2018-01-30&'
+       'sortBy=popularity&'
+       'apiKey=9c964579f7ea4465855d943ba98552f8')
+
+response = requests.get(url).json()
+
+print(response)
