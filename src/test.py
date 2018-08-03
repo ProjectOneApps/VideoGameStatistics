38df05b9ebe59a5f43e83a88994e8ff595d82dc2
Mayank Kishore <mayankkishore.in@gmail.com>
2018-08-03 15:25:01 -0500

    pytrends api

--- src/test.py
+++ src/test.py
@@ -1,6 +1,6 @@
 import requests
 import json
-import numpy
+import numpy as np
 import pandas as pd
 import sklearn
 import matplotlib
@@ -9,6 +9,7 @@ import scipy
 from pandas.io.json import json_normalize
 import news_api
 import datetime
+from pytrends.request import TrendReq
 
 news = news_api.ApiClient('9c964579f7ea4465855d943ba98552f8')
 
@@ -25,3 +26,9 @@ re = news.get_news_past_six_months(stringstart, stringend, 'fortnite')
 totalCount = re['totalResults']
 
 print('Results are {0}'.format(totalCount))
+
+# testing the google trends api
+pytrends = TrendReq()
+kw_list = ["Fortnite", "Minecraft", "Grand Theft Auto"]
+pytrends.build_payload(kw_list, cat=0, timeframe='all', geo='', gprop='')
+print(pytrends.get_historical_interest(kw_list, year_start=2017, month_start=9, day_start=1, hour_start=0, year_end=2018, month_end=8, day_end=1, hour_end=0, cat=0, geo='', gprop='', sleep=0))
