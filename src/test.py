e326d41c9542a27fb8b4ab30fc43129d178fc60a
Aman Kishore <amankishore105@gmail.com>
2018-08-03 03:42:16 -0400

    News API Initial Done

--- src/test.py
+++ src/test.py
@@ -8,19 +8,20 @@ import seaborn
 import scipy
 from pandas.io.json import json_normalize
 import news_api
-import twitter_api as ta
 import datetime
 
 news = news_api.ApiClient('9c964579f7ea4465855d943ba98552f8')
 
+start = datetime.date.today() - datetime.timedelta(days=30)  # gets the date from 30 days ago
+end = datetime.date.today() - datetime.timedelta(days=29)  # gets date from 29 days ago need to make into a for loop
+stringstart = start.strftime('%Y-%m-%d')                      # converts to a string
+stringend = start.strftime('%Y-%m-%d') 
 
-re = news.get_news_past_six_months('minecraft')
+re = news.get_news_past_six_months(stringstart, stringend, 'fortnite')
 
-data = json.loads(re)
-json_text = json.dumps(data)
-df = json_normalize(json_text)
+# data = json.loads(re)
+# json_text = json.dumps(data)
+# df = json_normalize(json_text)
+totalCount = re['totalResults']
 
-# print(df)
-
-d = datetime.date.today() - datetime.timedelta(days=30)
-print(d)
+print('Results are {0}'.format(totalCount))
